93911024a5cbb47d13d0f82c5b18984654bdac15
sangeetha <sangeethashree195@gmail.com>
2017-06-29 20:34:25 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+int s;
+Scanner sc=new Scanner(System.in);
+s=sc.nextInt();
+if(s>0)
+{
+System.out.println("The Number is Positive");
+}
+else 
+{
+System.out.println("The Number is Negative");
+}
+}
+}
