c7b4b7b8789a6173be9e49d096de7b096f95a9dc
sangeetha <sangeethashree195@gmail.com>
2017-06-30 16:32:18 +0530

    Create Fac.java

---
+++ Fac.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java .io.*;
+public class Fac{
+public static void main(String[] args){
+int a,i,fact=1;
+Scanner sc=new Scanner(system.in);
+System.out.println("enter the value");
+a=sc.nextInt();
+for(i=0;i<a;i++)
+{
+fact=i*fact;}
+System.out.println("the factorial is"+fact);
+}}
