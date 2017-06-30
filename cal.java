d7da3e2f9c9591f79c915877e8aba8b69f95fed5
sangeetha <sangeethashree195@gmail.com>
2017-06-30 19:18:41 +0530

    Create cal.java

---
+++ cal.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+int i,g,a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the table value");
+a=sc.nextInt();
+for(i=1;i<=10;i++)
+{
+g=i*a;
+System.out.println(a+"*"+i+"="+g);
+}
+}
+}
