e1060f51aa0b0a0b17c1dedf1d569ef7940b0c68
sangeetha <sangeethashree195@gmail.com>
2017-06-30 19:42:16 +0530

    Update Pall.java

--- Pall.java
+++ Pall.java
@@ -4,13 +4,13 @@ public class Pall{
 public static void main(String[] args){
 int a,t,n;
 int val=0;
-t=n
+t=n;
 Scanner sc=new Scanner(System.in);
 System.out.println("enter the number");
 n=sc.nextInt();
 while(n>0)
 {
-a=n%10
+a=n%10;
 n=n/10;
 }
 if(t==val)
