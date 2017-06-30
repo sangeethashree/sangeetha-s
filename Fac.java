7a52087f76fa5e12c35d72f5bc2f0ae4c222b920
sangeetha <sangeethashree195@gmail.com>
2017-06-30 19:07:38 +0530

    Update Fac.java

--- Fac.java
+++ Fac.java
@@ -6,7 +6,7 @@ int a,i,fact=1;
 Scanner sc=new Scanner(system.in);
 System.out.println("enter the value");
 a=sc.nextInt();
-for(i=0;i<a;i++)
+for(i=1;i<=a;i++)
 {
 fact=i*fact;}
 System.out.println("the factorial is"+fact);
