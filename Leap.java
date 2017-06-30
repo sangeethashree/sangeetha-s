85342325539a7fbc63b9f20c7f23f196540cfec0
sangeetha <sangeethashree195@gmail.com>
2017-06-30 15:49:14 +0530

    Update Leap.java

--- Leap.java
+++ Leap.java
@@ -4,8 +4,9 @@ public class Leap{
 public static void main(String [] args){
 int lp;
 Scanner sc=new Scanner(System.in);
+System.out.println("Enter the Given Year");
 lp=sc.nextInt();
-if((lp%4)&&(lp%100)==0)
+if((lp%4==0)&&(lp%100!=0)||(lp%400==0))
 {
 System.out.println("leap year");
 }
