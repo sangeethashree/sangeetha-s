b7a28399b7b1fd0ec9f7331f1bb09b3890b8adbd
sangeetha <sangeethashree195@gmail.com>
2017-06-30 19:38:46 +0530

    Create Pall.java

---
+++ Pall.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class Pall{
+public static void main(String[] args){
+int a,t,n;
+int val=0;
+t=n
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+n=sc.nextInt();
+while(n>0)
+{
+a=n%10
+n=n/10;
+}
+if(t==val)
+{
+System.out.println("the given number is a palindrome");
+}
+else
+{
+System.out.println("the given number is not a palindrome");
+}
+}
+}
