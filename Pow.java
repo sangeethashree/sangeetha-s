8417e58aad7d9301b39085d8cd7db3473c4477c5
sangeetha <sangeethashree195@gmail.com>
2017-07-02 20:17:58 +0530

    Create Pow.java

---
+++ Pow.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Pow
+{
+public static void main(Strings[] args)
+{
+int b,e,res;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the base and exponent");
+b=sc.nextInt();
+e=sc.next.Int();
+ res=pow(b,e);
+System.out.println("the result is"+res);
+}
+}
