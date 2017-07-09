3122a76f966e8f4792cb2133dbf2defbbc1b6d74
sangeetha <sangeethashree195@gmail.com>
2017-07-09 18:29:43 +0530

    Create Up.java

---
+++ Up.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Up{
+public static void main(String[] args)
+{
+String name="hAi";
+string name=name.toUppercase();
+System.out.println(name);
+}
+}
