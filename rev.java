5025020a45e871a1c1dd04421eae26a1ba8608b9
sangeetha <sangeethashree195@gmail.com>
2017-07-09 18:47:13 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class rev{
+public static void main(String[] args)
+{
+String buffer s1="hello world";
+ System.out.println(" the result is"+s1.reverse());
+ }
+ }
