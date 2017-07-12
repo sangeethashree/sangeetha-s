d88724f58f89b20e20d5ad9c04cd22e2c6ab669f
sangeetha <sangeethashree195@gmail.com>
2017-07-12 10:46:49 +0530

    Update Up.java

--- Up.java
+++ Up.java
@@ -3,7 +3,9 @@ import java.io.*;
  class Up{
 public static void main(String[] args)
 {
-String name="hAi";
+ String name=new String();
+Scanner sc=new scanner(system.in);
+ name=sc.nextLINE();
 name=name.toUpperCase();
 System.out.println(name);
 }
