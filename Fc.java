953f0168ca8d3d556ca2e451e1044733dcbb9fbd
sangeetha <sangeethashree195@gmail.com>
2017-07-23 11:51:25 +0530

    Update Fc.java

--- Fc.java
+++ Fc.java
@@ -1,20 +1,16 @@
 import java.util.*;
 import java .io.*;
-public class Fc{
+ class Fc{
 public static void main(String[] args){
 int a,i,fact=1;
-Scanner sc=new Scanner(system.in);
+Scanner sc=new Scanner(System.in);
 System.out.println("enter the value");
 a=sc.nextInt();
 for(i=1;i<=a;i++)
 {
 fact=i*fact;
-}if(n!=0)
-{
+}
 System.out.println("the factorial is"+fact);
-}
-  else
-    System.out.println("the  factorial is 0");
-}
+
 }}
   
