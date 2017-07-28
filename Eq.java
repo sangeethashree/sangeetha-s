0a77f7d98e7ff0e613dc4d7cfeb7809135b0a5c7
sangeetha <sangeethashree195@gmail.com>
2017-07-28 12:38:37 +0530

    Create Eq.java

---
+++ Eq.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Eq{
+public static void main(String args[])
+{
+String word1;
+String word2;
+Scanner sc=new Scanner(System.in);
+word1=sc.next();
+word2=sc.next();
+String result;
+result=word1+word2;
+System.out.println(result);
+}
+}
