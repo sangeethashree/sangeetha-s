5a4df0d4f66a00118f0918f64097b3aa66546700
sangeetha <sangeethashree195@gmail.com>
2017-08-02 11:04:46 +0530

    Create Reversee.java

---
+++ Reversee.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Reversee{
+public static void main(string[]args){
+string=s1;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the string");
+s1=sc.next();
+System.out.println("the reverse string is"+s1.reverse());
+}
+}
