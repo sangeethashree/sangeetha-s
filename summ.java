64310dffd5c1108f89d3fa4da218af78cb76c2f8
sangeetha <sangeethashree195@gmail.com>
2017-08-02 10:45:00 +0530

    Create summ.java

---
+++ summ.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class summ{
+public static void main(String args[])
+{
+int a,total=0;
+Scanner sc=new Scanner(System.in);
+a=sc.next.Int();
+for(int i=0;i<n;i++)
+{
+total=+i
+}
+System.out.println("The sum of the natural numbers are"+total);
+}
+}
