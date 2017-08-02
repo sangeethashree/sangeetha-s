933d9c6066241c9f65e12f8893fd4075e7e11037
sangeetha <sangeethashree195@gmail.com>
2017-08-02 11:15:35 +0530

    Update Reversee.java

--- Reversee.java
+++ Reversee.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import java.util.*;
 class Reversee{
-public static void main(string[]args){
-string=s1;
+public static void main(String[]args){
+StringBuffer sb=new StringBuffer();  
 Scanner sc=new Scanner(System.in);
 System.out.println("enter the string");
-s1=sc.next();
+sb.append(input.nextLine());
 System.out.println("the reverse string is"+s1.reverse());
 }
 }
