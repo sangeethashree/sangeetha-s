38e6d231d6eedbb68cea71e8ca8df697370edee4
sangeetha <sangeethashree195@gmail.com>
2017-08-10 09:04:32 +0530

    Update rev.java

--- rev.java
+++ rev.java
@@ -3,7 +3,10 @@ import java.io.*;
 public class rev{
 public static void main(String[] args)
 {
-String buffer s1="hello world";
+StringBuffer s1 = new StringBuffer();
+Scanner in = new Scanner(System.in);
+System.out.println("Enter a string");
+s1.append(in.nextLine());
  System.out.println(" the result is"+s1.reverse());
  }
  }
