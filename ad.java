a842596302a2f744d3ce3ff98be8fd71277f6373
sangeetha <sangeethashree195@gmail.com>
2017-08-30 11:01:25 +0530

    Create ad.java

---
+++ ad.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class ad
+{
+    public static void main(String args[])
+{
+    int a=80;
+    int b=90;
+    int c=0;
+    for(int i=0;i<=b;i++)
+    {
+    c=a;
+    a++;
+    
+    }
+    System.out.println(c);
+}
+}
