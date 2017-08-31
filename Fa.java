37be8ff7fa4d487e55821667ec2bb78cf75a6ca6
sangeetha <sangeethashree195@gmail.com>
2017-08-31 09:11:27 +0530

    Update Fa.java

--- Fa.java
+++ Fa.java
@@ -4,10 +4,10 @@ import java.util.*;
 public static void main(String[] args) {
 double celsius,fahrenheat;
 Scanner sc=new Scanner(System.in);
-System.out.print("Please enter temperature in celsius : ");      
+System.out.print("enter temperature(celsius) = ");      
 celsius =sc.nextDouble();
  fahrenheat = (celsius*9.0/5.0+32);
- System.out.println("Temperature in fahrenheat is : "+fahrenheat);
+ System.out.println("Temperature is(fahrenheat) = "+fahrenheat);
 
 }
 }
