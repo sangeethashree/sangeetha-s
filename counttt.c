7b0e09cb8ec21ee8981d44da4ef06b62b03329b6
sangeetha <sangeethashree195@gmail.com>
2017-09-14 09:13:46 +0530

    Create counttt.c

---
+++ counttt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int length,j,wrd=1;
+    char string[50];
+    clrscr();
+    printf("***provide string***");
+    gets(a);
+    length=strlen(a);
+    for(j=0;j<length;j++)
+    {
+        if(a[j]=='    '&&a[i+1]=='    ')
+        wrd=wrd+1;
+    }
+    printf("%d words"wrd);
+    getch();
+}
