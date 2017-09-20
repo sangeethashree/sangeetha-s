bf7bf7809e6acdc350318e534b9efd292829f2d2
sangeetha <sangeethashree195@gmail.com>
2017-09-20 11:14:10 +0530

    Update cccccccc.c

--- cccccccc.c
+++ cccccccc.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<string.h>
-void main()
+int main()
 {
     int length,j,wrd=1;
     char string[50];
     
     printf("***provide string***");
-    scanf("%s",string[j]);
+    scanf("%s",string);
     length=strlen(string);
     for(j=0;j<length;j++)
     {
         if(string[j]==''&&string[j+1]=='')
         wrd=wrd+1;
     }
-    printf("%d words"wrd);
+    printf("%d words",wrd);
     return 0;
 }
